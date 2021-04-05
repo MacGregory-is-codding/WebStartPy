fe8fadfb0ec0a979c27ec872a892e5965d36af88
Andrii <andrey.skorochod@gmail.com>
2021-04-05 20:36:18 +0300

    tabulation fixed

--- webstart_wc.py
+++ webstart_wc.py
@@ -24,37 +24,33 @@ if __name__ == '__main__':
     content = {
         #index.html
         'index' : 
-        """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>WebStart</title>
-            <link rel="stylesheet" href="assets/css/style.css">
-        </head>
-        <body>
-            
-            <script src="assets/js/script.js"></script>
-        </body>
-        </html>
-        """,
+"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebStart</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
+""",
 
         #style.css
         'style': 
-        """
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }     
-        """,
+"""* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}     
+""",
 
         #script.js
-        'script' : """
-            'use strict'
-        """,
+        'script' : "'use strict'",
     }
 # HARDCODE END - I JUST WANTED TO MAKE IT IN ONE FILE)
 
@@ -83,7 +79,7 @@ if __name__ == '__main__':
         try:
             if not os.path.exists(file.path):
                 os.makedirs(file.path)
-            with open(os.path.join(file.path, file.name), 'x') as fout:
+            with open(os.path.join(file.path, file.name), 'w') as fout:
                 fout.write(file.content)
         except OSError as e:
             print('Ooopss... %s' %e)
